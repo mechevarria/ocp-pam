f1a944c31281c9b4cb6ea691b8c1a53ec5f69686
Mike Echevarria <mechevar@redhat.com>
2019-05-23 14:51:00 -0400

    old scripts that might be used if sso is working

--- unused-scripts/ocp-deploy-sso.sh
+++ unused-scripts/ocp-deploy-sso.sh
@@ -16,7 +16,7 @@ fi
 oc policy add-role-to-user view system:serviceaccount:$(oc project -q):default
 
 # using custom template to reference theme image stream and force using openshift namespace for postgresql
-oc new-app --template=sso73-x509-postgresql-persistent \
+oc new-app --template=sso72-x509-postgresql-persistent \
  -p SSO_ADMIN_USERNAME="admin" \
  -p SSO_ADMIN_PASSWORD="redhatsso1!" \
  -p SSO_REALM="pam-realm"
