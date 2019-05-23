a993ee5ff897b0226cbdff6587ead0c394c57a29
Mike Echevarria <mechevar@redhat.com>
2019-05-23 14:53:49 -0400

    add task methods

--- src/app/kie.service.ts
+++ src/app/kie.service.ts
@@ -4,35 +4,38 @@ import { catchError } from 'rxjs/operators';
 import { MessageService } from './message/message.service';
 import { Observable, of } from 'rxjs/index';
 
-const headers: HttpHeaders = new HttpHeaders().append('Content-Type', 'application/json').append('Authorization', 'Basic ' + btoa('pamAdmin:redhatpam1!'));
+const headers: HttpHeaders = new HttpHeaders()
+  .append('Content-Type', 'application/json')
+  .append('Authorization', 'Basic ' + btoa('pamAdmin:redhatpam1!'));
 
 const httpOptions = {
   headers: headers
 };
 const baseUrl = '/services/rest';
-const processId = 'approval';
-const containerId = 'lease_1.0.0-SNAPSHOT';
+const processId = 'malware.inspect';
+const containerId = 'malware_1.0.0-SNAPSHOT';
 
 @Injectable({
   providedIn: 'root'
 })
 export class KieService {
-  constructor(private messageService: MessageService, private http: HttpClient) {}
+  constructor(private messageService: MessageService, private http: HttpClient) { }
 
-  process(id: number, annualRent: number): Observable<any> {
+  startProcess(filename: string): Observable<any> {
     const url = `${baseUrl}/server/containers/${containerId}/processes/${processId}/instances`;
 
     const body = {
-      id: id,
-      lease: {
-        'com.myspace.restcall.LeaseModel': {
-          annualRent: annualRent,
-          isAutoApproved: false
-        }
-      }
+      filename: filename
     };
     return this.http.post<any>(url, body, httpOptions).pipe(
       catchError(res => this.handleError('process()', res))
+    );
+  }
+
+  getProcesses(): Observable<any> {
+    const url = `${baseUrl}/server/containers/${containerId}/processes/${processId}/instances`;
+    return this.http.get<any>(url, httpOptions).pipe(
+      catchError(res => this.handleError('getProcesses()', res))
     );
   }
 
@@ -43,34 +46,31 @@ export class KieService {
     );
   }
 
-  getTask(taskId: number): Observable<any> {
-    const url = `${baseUrl}/server/queries/tasks/instances/${taskId}`;
+  getTask(taskInstanceId: number): Observable<any> {
+    const url = `${baseUrl}/server/containers/${containerId}/tasks/${taskInstanceId}/contents/input`;
     return this.http.get<any>(url, httpOptions).pipe(
       catchError(res => this.handleError('getTask()', res))
     );
   }
 
-  claim(taskId: number): Observable<any> {
-    const url = `${baseUrl}/server/containers/${containerId}/tasks/${taskId}/states/claimed`;
+  claim(taskInstanceId: number): Observable<any> {
+    const url = `${baseUrl}/server/containers/${containerId}/tasks/${taskInstanceId}/states/claimed`;
     return this.http.put<any>(url, null, httpOptions).pipe(
       catchError(res => this.handleError('claim()', res))
     );
   }
 
-  start(taskId: number): Observable<any> {
-    const url = `${baseUrl}/server/containers/${containerId}/tasks/${taskId}/states/started`;
+  start(taskInstanceId: number): Observable<any> {
+    const url = `${baseUrl}/server/containers/${containerId}/tasks/${taskInstanceId}/states/started`;
     return this.http.put<any>(url, null, httpOptions).pipe(
       catchError(res => this.handleError('start()', res))
     );
   }
 
-  complete(taskId: number, status: string): Observable<any> {
-    const url = `${baseUrl}/server/containers/${containerId}/tasks/${taskId}/states/completed`;
-    const body = {
-      updatedstatus: status
-    };
+  complete(taskInstanceId: number): Observable<any> {
+    const url = `${baseUrl}/server/containers/${containerId}/tasks/${taskInstanceId}/states/completed`;
 
-    return this.http.put<any>(url, body, httpOptions).pipe(
+    return this.http.put<any>(url, null, httpOptions).pipe(
       catchError(res => this.handleError('complete()', res))
     );
   }
