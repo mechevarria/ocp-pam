5892d5db4f87e0eebf7a01c9a83931112161cf84
Mike Echevarria <mechevar@redhat.com>
2019-05-23 14:50:43 -0400

    add approve component

---
+++ src/app/approve/approve.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { KieService } from '../kie.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MessageService } from '../message/message.service';
+
+@Component({
+  selector: 'app-approve',
+  templateUrl: './approve.component.html',
+  styleUrls: ['./approve.component.css']
+})
+export class ApproveComponent implements OnInit {
+  task: any = null;
+  taskInstanceId: number;
+
+  constructor(private kieService: KieService, private route: ActivatedRoute, private messageService: MessageService, private router: Router) { }
+
+  load(taskId: number): void {
+    this.kieService.getTask(taskId).subscribe(task => {
+      this.task = task;
+    });
+  }
+
+  approve(): void {
+    this.kieService.complete(this.taskInstanceId).subscribe(() => {
+      this.messageService.info('Results Approved');
+      this.router.navigate(['/home/task']);
+    });
+  }
+
+
+  ngOnInit() {
+    this.route.queryParams.subscribe(routeParams => {
+      if (routeParams.id) {
+        this.taskInstanceId = routeParams.id;
+        this.load(this.taskInstanceId);
+      }
+    });
+  }
+
+}
